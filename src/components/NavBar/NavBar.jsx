aacf09c40d8adf49ee9ea073818b3092fe29ba26
Morgan Hewitt <morganhewitt0211@gmail.com>
2025-10-05 16:17:42 +0200

    First draft of the nav bar UI

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,15 @@
 import Logo from "./Logo";
 import ActivityButtons from "./ActivityButtons";
+import NavBarButtons from "./NavBarButtons";
 
 function NavBar() {
   return (
-    <div className="flex">
-      <Logo />
-      <ActivityButtons />
+    <div className="flex items-center justify-between p-4 bg-white shadow-md">
+      <div className="flex items-center gap-4">
+        <Logo />
+        <ActivityButtons />
+      </div>
+      <NavBarButtons />
     </div>
   );
 }
---
+++ src/components/NavBar/NavBarButtons.jsx
@@ -0,0 +1,21 @@
+/*
+create event
+find event
+my events / calendar
+log in
+sign up
+*/
+
+function NavBarButtons() {
+  return (
+    <div className="flex flex-row gap-15 p-5">
+      <button>create event</button>
+      <button>find event</button>
+      <button>my events & calendar</button>
+      <button>log in</button>
+      <button>sign up</button>
+    </div>
+  );
+}
+
+export default NavBarButtons;
