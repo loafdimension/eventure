330310cb8487739a6a85bdd0f3ec59aab86a5cd9
Morgan Hewitt <morganhewitt0211@gmail.com>
2025-10-13 11:29:11 +0200

    Formatting create event page

--- src/pages/Create-Event.jsx
+++ src/pages/Create-Event.jsx
@@ -5,15 +5,15 @@ function CreateEvent() {
     <>
       <NavBar />
 
-      <form> 
+      <form className="ml-65 mr-65 mt-10"> 
         <div className="flex flex-col gap-4 p-5">
           <input
             type="text"
             placeholder="Event Title"
-            className="ml-3 mt-2 text-2xl font-bold"
+            className="mt-2 text-2xl font-bold"
           ></input>
-          <p className="ml-3 text-lg font-semibold">Activity Type: </p>
-          <div className="flex gap-5 ml-3 text-lg">
+          <p className="text-lg font-semibold">Activity Type: </p>
+          <div className="flex gap-5 text-lg">
             <label className="flex gap-2">
               <input type="checkbox" name="Activities" value="Walk"></input>
               Walk
@@ -64,7 +64,7 @@ function CreateEvent() {
           ></input>
           <button
             type="submit"
-            className="bg-blue-600 text-white font-medium py-3 px-6 rounded-lg hover:bg-blue-700 text-lg transition"
+            className="bg-[#FFDD00] text-white font-medium py-3 px-6 rounded-lg hover:bg-[#EBBE0C] text-lg transition"
           >
             Create
           </button>
